3a09c4d13d85c4d0cd43f4730c56092a54190000
Uli Hansen <dev@familie-hansen.name>
2020-02-25 23:11:32 +0100

    spring boot starter

---
+++ src/main/java/com/hanulhan/finanzen/FinanzenApplication.java
@@ -0,0 +1,13 @@
+package com.hanulhan.finanzen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanzenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanzenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hanulhan/finanzen/FinanzenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanulhan.finanzen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanzenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
